5146ccd0525e1cd055aa1ad9524cc6bfedad46ca
EduardoMay <eduardo_may@outlook.com>
2021-03-19 11:54:14 -0500

    feat: crear archivo types

--- src/store.ts
+++ src/store.ts
@@ -1,6 +1,7 @@
 import { createStore } from "vuex";
 import { deleteLabel, getLabels, saveLabel } from "./helpers/labels";
 import { LabelInterface } from "@/interfaces/LabelInterface";
+import { types } from "@/types/types";
 
 export const store = createStore({
   state() {
@@ -12,21 +13,21 @@ export const store = createStore({
   // ACTIONS (asynchronous)
   actions: {
     addLabel({ commit }, payload) {
-      commit("addLabel", payload);
+      commit(types.ADD_LABEL, payload);
     },
     deleteLabel({ commit }, payload) {
-      commit("deleteLabel", payload);
+      commit(types.DELETE_LABEL, payload);
     }
   },
 
   // MUTATIONS (set the state)
   mutations: {
-    addLabel(state: any, payload: any) {
+    [types.ADD_LABEL](state: any, payload: any) {
       state.notesLabels = [...state.notesLabels, payload.label];
 
       saveLabel(payload.label);
     },
-    deleteLabel(state: any, payload: any): void {
+    [types.DELETE_LABEL](state: any, payload: any): void {
       state.notesLabels = state.notesLabels.filter(
         (e: LabelInterface) => e.id !== payload.label.id
       );
---
+++ src/types/types.ts
@@ -0,0 +1,6 @@
+export const types = {
+  ADD_LABEL: "addLabel",
+  GET_LABELS: "getLabels",
+  DELETE_LABEL: "deleteLabel",
+  UPDATE_LABEL: "updateLabel"
+};
