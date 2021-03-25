4356af40c8b348b02ca9c81b3caeaaeeea2d7153
EduardoMay <eduardo_may@outlook.com>
2021-03-25 11:37:14 -0500

    fix: error al entrar a la app

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,13 +1,5 @@
-import { Store } from "@/store";
-
 declare module "*.vue" {
   import { defineComponent } from "vue";
   const component: ReturnType<typeof defineComponent>;
   export default component;
-}
-
-declare module "@vue/runtime-core" {
-  interface ComponentCustomProperties {
-    $store: Store;
-  }
 }
