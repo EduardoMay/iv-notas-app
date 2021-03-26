3cf1fd9b22e9ebca1aa3f43fc0a997574a3cc36c
EduardoMay <eduardo_may@outlook.com>
2021-03-25 20:59:13 -0500

    fix: volver a crear funcion

--- src/helpers/labels.ts
+++ src/helpers/labels.ts
@@ -5,22 +5,69 @@ import { NoteInterface } from "@/interfaces/NoteInterface";
 
 import { getNotes } from "./notes";
 
+/**
+ * Get Labels
+ *
+ * @return  {Array<LabelInterface>}
+ */
 export const getLabels = (): Array<LabelInterface> => {
 	return localStorage.notesLabels ? JSON.parse(localStorage.notesLabels) : [];
 };
 
+/**
+ * Get Label By Id
+ *
+ * @param   {number}          id
+ *
+ * @return  {LabelInterface}
+ */
+export const getLabelById = (id: number): LabelInterface => {
+	const label: LabelInterface = { id: 0, name: "", color: "#92949c" };
+
+	if (id === 0) return label;
+
+	const obj = _.find(getLabels(), { id });
+
+	if (obj) {
+		return obj;
+	} else {
+		return label;
+	}
+};
+
+/**
+ * Save Label
+ *
+ * @param   {LabelInterface}  label
+ *
+ * @return  {void}
+ */
 export const saveLabel = (label: LabelInterface): void => {
 	const labels = [...getLabels(), label];
 
 	localStorage.setItem("notesLabels", JSON.stringify(labels));
 };
 
+/**
+ * Delete Label
+ *
+ * @param   {LabelInterface}  label
+ *
+ * @return  {void}
+ */
 export const deleteLabel = (label: LabelInterface): void => {
 	const labels = getLabels().filter((e) => e.id !== label.id);
 
 	localStorage.setItem("notesLabels", JSON.stringify(labels));
 };
 
+/**
+ * Select Notes By Id Label
+ *
+ * @param   {number[]}         id
+ *
+ * @return  {NoteInterface[]}
+ */
 export const selectNotesByIdLabel = (id: number): NoteInterface[] => {
 	return getNotes().filter((note) => note.label === id);
 };
